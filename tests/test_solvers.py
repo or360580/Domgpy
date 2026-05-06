b667b94be75e5da2e92f8632c2e5e62c90124b2a
Guillermo_Ortega.R <256671120+or360580@users.noreply.github.com>
2026-05-06 01:34:52 +0000

    Empezamos a meter notas para identificar lo que hace cada parte de nuestro codigo

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -1,5 +1,5 @@
 from Dompy.solvers import solve_example
-
+"""Este  es un codigo de prueba donde definimos solo una funcion que nos genera de forma aleatoria una frafica, y en base a este buscamos el conjunto y el numero de dominacion."""
 def test_solve_example_empty():
     assert solve_example([]) == [
         
@@ -11,23 +11,29 @@ def test_solve_example_data():
 
 
 
-
-def analizar_grafica_aleatoria(n_nodos, probabilidad):
-    G = nx.erdos_renyi_graph(n_nodos, probabilidad)
+"""Aqui definimos nuestra funcion de analisis de grafica que depende de unicamente los nodos y las aristas"""
+def an_graf_aleat(n_nodos, aristas):
+    """Aqui la grafica la llamamos G, para generar la grafica usamos un algoritmo Erdos-Renyi que viene en la libreria de networkx """
+    G = nx.erdos_renyi_graph(n_nodos, aristas)
     
     print(f"Grafica generada con {n_nodos} nodos y {G.number_of_edges()} aristas.")
     print("-" * 30)
+    """el siguiente codigo manda a llamar un algoritmo que ya es contenido en la libreria Networkx"""
     conjunto_dominacion = nx.dominating_set(G)
+    """El siguiente codigo ahora solamente hace el conteo de los elementos que estan en el conjunto de dominacion"""
     numero_dominacion = len(conjunto_dominacion)
+    """Con los dos codigos anteriores obtenemos el conjunto de dominacion y el numero de dominacion."""
+
     
     print(f"Conjunto de Dominación: {conjunto_dominacion}")
     print(f"Número de Dominación (γ): {numero_dominacion}")
     print("-" * 30)
 
+    """Este codigo esta demas sin embargo lo quise considerar para codigo de prueba en el grafico, donde nos da el numero cromatico"""
     dict_colores = nx.coloring.greedy_color(G, strategy="largest_first")
     num_cromatico = max(dict_colores.values()) + 1
     
     print(f"Número Cromático aproximado (χ): {num_cromatico}")
     print(f"Asignación de colores: {dict_colores}")
-
-analizar_grafica_aleatoria(10, 0.3)
+"""Por ultimo qui tenemos la designacion de cuantos nodos y aristas tendra nuestro grafo o grafica"""
+an_graf_aleat(13, 0.3)
